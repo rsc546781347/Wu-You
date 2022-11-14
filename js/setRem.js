795dd0bf92059930eeb7a00153af3e6f94900fdc
任苏成 <546781347@qq.com>
2022-11-14 19:41:38 +0800

    '创建'

--- js/setRem.js
+++ js/setRem.js
@@ -6,13 +6,13 @@ function setRem() {
     //设置最大最小宽度
     client_w = client_w > 625 ? 625 : client_w;
     client_w = client_w < 300 ? 300 : client_w;
-    console.log(ui_w, client_w);
+    // console.log(ui_w, client_w);
 
     var html = document.getElementsByTagName("html")[0];
     html.style.fontSize = (client_w / ui_w) * 10 + 'px';
-
-
 }
+// 当页面加载的时候  也要调用一下setRem 目的  默认 把html 根节点的字体大小 设置为10px
+window.onload = setRem;
 
 var flg = true;
 window.onresize = function() {
